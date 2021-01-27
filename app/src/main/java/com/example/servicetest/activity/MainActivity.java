617bae61b2b4ea62584ca4294c2eda58189814b5
yuanbao97 <316261393@qq.com>
2021-01-27 17:32:49 +0800

    10.3.3 活动和服务进行通信

--- app/src/main/java/com/example/servicetest/activity/MainActivity.java
+++ app/src/main/java/com/example/servicetest/activity/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.servicetest.activity;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,10 +20,33 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     @BindView(R.id.start_service_button)
     Button startServiceButton;
     @BindView(R.id.stop_service_button)
     Button stopServiceButton;
+    @BindView(R.id.bind_service_button)
+    Button bindServiceButton;
+    @BindView(R.id.unbind_service_button)
+    Button unbindServiceButton;
+
+    private MyService.DownloadBinder mDownloadBinder;
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mDownloadBinder = (MyService.DownloadBinder) service;
+            mDownloadBinder.startDownload();
+            mDownloadBinder.getProgress();
+            Log.e(TAG, "onServiceConnected executed");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.e(TAG, "onServiceDisconnected executed");
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
     }
 
-    @OnClick({R.id.start_service_button, R.id.stop_service_button})
+    @OnClick({R.id.start_service_button, R.id.stop_service_button, R.id.bind_service_button, R.id.unbind_service_button})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.start_service_button:
@@ -39,6 +66,14 @@ public class MainActivity extends AppCompatActivity {
                 Intent stopIntent = new Intent(this, MyService.class);
                 stopService(stopIntent);
                 break;
+            case R.id.bind_service_button:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, mConnection, BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service_button:
+                unbindService(mConnection);
+                break;
         }
     }
+
 }
--- app/src/main/java/com/example/servicetest/service/MyService.java
+++ app/src/main/java/com/example/servicetest/service/MyService.java
@@ -2,20 +2,33 @@ package com.example.servicetest.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 public class MyService extends Service {
 
+    public class DownloadBinder extends Binder {
+
+        public void startDownload() {
+            Log.e(TAG, "startDownload executed");
+        }
+
+        public int getProgress() {
+            Log.e(TAG, "getProgress executed");
+            return 0;
+        }
+    }
+
     private static final String TAG = "MyService";
+    private DownloadBinder mBinder = new DownloadBinder();
 
     public MyService() {
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 
     @Override
