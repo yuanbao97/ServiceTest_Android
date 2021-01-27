e6f24000ac332c3450f34d16f3231212d2e83a1b
yuanbao97 <316261393@qq.com>
2021-01-27 16:54:16 +0800

    10.3.2 启动和停止服务

--- app/src/main/java/com/example/servicetest/activity/MainActivity.java
+++ app/src/main/java/com/example/servicetest/activity/MainActivity.java
@@ -1,16 +1,44 @@
 package com.example.servicetest.activity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.servicetest.R;
+import com.example.servicetest.service.MyService;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.start_service_button)
+    Button startServiceButton;
+    @BindView(R.id.stop_service_button)
+    Button stopServiceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.start_service_button, R.id.stop_service_button})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_service_button:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service_button:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);
+                break;
+        }
     }
 }
